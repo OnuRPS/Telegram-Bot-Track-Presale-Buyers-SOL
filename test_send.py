5350c82ed849d86fa1079baea528aa415b8428fd
OnuRPS <124206152+OnuRPS@users.noreply.github.com>
2025-05-07 00:57:14 +0200

    Delete test_send.py

--- test_send.py
+++
@@ -1,9 +0,0 @@
-import os
-from telegram import Bot
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-bot = Bot(token=TELEGRAM_TOKEN)
-
-bot.send_message(chat_id=CHAT_ID, text="✅ Telegram bot test: funcționez corect!")
